6c73e86c187ed2e3d55dfc70755ea38637664cd8
Rodrigo Marques Ribeiro <dygaomail@gmail.com>
2017-09-29 08:10:03 -0300

    Mostrando animação de carregamento e mensagem de erro

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,19 +1,23 @@
-botaoShuffle.click(function() {
-
-
-});
+botaoShuffle.click(atualizaJogo);
 
 /**
  * Função para buscar uma nova frase no banco de dados
  * e atualizar os contadores do jogo
- * 
+ *
  * @param  {object} retorno recebe o objeto com as
  * informações da nova frase
  */
-function atualizaJogo(retorno) {
+function atualizaJogo() {
+
+    /* Armazenando elementos */
+    var spinner = $('#spinner');
+    var erro = $('#erro');
+
+    /* Mostrando animação de carregamento */
+    spinner.toggle();
 
     /* Buscando nova frase */
-    $.get('http://localhost:3000/frases', function() {
+    $.get('http://localhost:3000/frases', function(retorno) {
 
         /* Gerando número aleatório */
         var numeroAleatorio = Math.floor(Math.random() * retorno.length);
@@ -28,6 +32,21 @@ function atualizaJogo(retorno) {
 
         /* Reiniciando o jogo depois de atualizar a frase */
         reiniciaJogo();
+
+    }).fail(function() {
+
+        /* Mostrando erro */
+        erro.toggle();
+
+        /* Escondendo mensagem de erro depois de um tempo */
+        setTimeout(function() {
+            erro.toggle();
+        }, 2000);
+
+    }).always(function() {
+
+        /* Escondendo animação de carregamento assim que a requisição termina */
+        spinner.toggle();
 
     });
 
